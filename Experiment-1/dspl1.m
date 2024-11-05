216707713d8b9c3a29de16ee58619f5e1a73881c
Annamaryjames <137596558+Annamaryjames@users.noreply.github.com>
2024-11-05 18:40:20 +0530

    Update and rename dspl1.m to Experiment-1/dspl1.m

--- Experiment-1/dspl1.m
+++ Experiment-1/dspl1.m
@@ -1,11 +1,11 @@
-%% 
-% EXPERIMENT NO-1
-% 
+%% EXPERIMENT NO-1
+
 % Simulation of basic test signals
 
 clc;
 clear all;
 close all;
+
 %unit impulse
 t=-5:1:5;
 y1=[zeros(1,5),ones(1,1),zeros(1,5)];
@@ -14,6 +14,7 @@ stem(t,y1);
 title('unit impulse');
 xlabel('time index');
 ylabel('amplitude');
+
 %unit step
 y2=[zeros(1,5),ones(1,6)];
 subplot(3,3,2);
@@ -21,6 +22,7 @@ stem(t,y2);
 title('unit step');
 xlabel('time index');
 ylabel('amplitude');
+
 %ramp
 t3=0:1:10;
 y3=[t3];
@@ -32,6 +34,7 @@ title('ramp');
 xlabel('time index');
 ylabel('amplitude');
 legend("discrete","continuous");
+
 %sine wave
 t4=0:0.01:1;
 f4=4;
@@ -43,6 +46,7 @@ title('sine wave');
 xlabel('time index');
 ylabel('amplitude');
 legend("discrete","continuous");
+
 %cosine wave
 subplot(3,3,5);
 stem(t4,cos(2*pi*f4*t4));
@@ -52,6 +56,7 @@ title('cosine wave');
 xlabel('time index');
 ylabel('amplitude');
 legend("discrete","continuous");
+
 %squarewave-bipolar
 t6=0:0.0001:1;
 f6=10;
@@ -60,12 +65,14 @@ plot(t6,square(2*pi*f6*t6));
 title('squarewave-bipolar');
 xlabel('time index');
 ylabel('amplitude');
+
 %squarewave-unipolar
 subplot(3,3,7);
 plot(t6,sqrt(square(2*pi*f6*t6)));
 title('squarewave-unipolar');
 xlabel('time index');
 ylabel('amplitude');
+
 %triangular wave
 t8=0:0.25:10;
 f8=5;
@@ -77,6 +84,7 @@ title('triangular wave');
 xlabel('time index');
 ylabel('amplitude');
 legend("discrete","continuous");
+
 %exponential signal
 t9=0:0.01:100;
 y9=exp(t9);
