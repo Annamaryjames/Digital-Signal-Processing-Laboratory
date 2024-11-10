5f7be6b9455e144b82f51a590d0639131873ebb9
Annamaryjames <137596558+Annamaryjames@users.noreply.github.com>
2024-11-10 19:08:19 +0530

    Add files via upload

---
+++ Experiment-11/dspl11.c
@@ -0,0 +1,16 @@
+
+% EXPERIMENT NO-11
+
+% Generation of sine wave using DSP Kit
+
+#include<stdio.h>
+#include<math.h>
+#define pi 3.14159
+float s[100];
+void main()
+{
+int i;
+float f=100,Fs=10000
+for(i=0;i<100;i++)
+s[i]=sin(2*pi*f*i/Fs)
+}
